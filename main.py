f645f3714a714c317db5e47aa7e4ecca831cf8b2
Fernando Camaro Nogues <fernando@ascent.ai>
2020-02-24 19:09:54 +0900

    several modifications. Most important is that the discriminator doesn't start training after a given number of episodes Other modifications: log every 1K updates redefine agent reward to be logged if adiayn log skill and agent id save checkpoint every 2K steps

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from sac import SAC
 from model import Discriminator
 from tensorboardX import SummaryWriter
 from replay_memory import ReplayMemory
+import pybullet
+import pybullet_envs
+import time
 
 parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
 parser.add_argument('--env-name', default="HalfCheetah-v2",
@@ -34,7 +37,7 @@ parser.add_argument('--seed', type=int, default=123456, metavar='N',
 parser.add_argument('--batch_size', type=int, default=256, metavar='N',
                     help='batch size (default: 256)')
 parser.add_argument('--num_steps', type=int, default=1000001, metavar='N',
-                    help='maximum number of steps (default: 1000000)')
+                    help='maximum number of steps (default: 10000000)')
 parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
                     help='hidden size (default: 256)')
 parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
@@ -51,6 +54,8 @@ parser.add_argument('--diayn', action="store_true",
                     help='do diayn (default: False)')
 parser.add_argument('--num_skills', type=int, default=2, metavar='N',
                     help='number of skills for diayn (default: 2)')
+parser.add_argument('--disc_start_epi', type=int, default=500, metavar='N',
+                    help='episode in which the discriminator starts being trained (default: 500)')
 args = parser.parse_args()
 
 # Environment
@@ -59,6 +64,7 @@ env = gym.make(args.env_name)
 torch.manual_seed(args.seed)
 np.random.seed(args.seed)
 env.seed(args.seed)
+pybullet_env = "Bullet" in args.env_name
 
 # Agents
 agent0 = SAC(env.observation_space.shape[0], env.action_space, args, handicaped=False)
@@ -83,6 +89,10 @@ for i_episode in itertools.count(1):
     episode_steps = 0
     done = False
     state = env.reset()
+    if pybullet_env:
+        pybullet.setRealTimeSimulation(1)
+        time.sleep(1)
+        pybullet.setRealTimeSimulation(0)
     # sample skill
     if args.diayn:
         skill = np.random.randint(args.num_skills)
@@ -106,7 +116,7 @@ for i_episode in itertools.count(1):
                 critic_1_loss_1, critic_2_loss_1, policy_loss_1, ent_loss_1, alpha_1 = agent1.update_parameters(memory, args.batch_size, updates, discriminator)
                 
                 # update discriminator
-                if args.diayn:
+                if args.diayn and i_episode > args.disc_start_epi:
                     _, _, _, next_state, _, skills, agent_ids = memory.sample(batch_size=args.batch_size)
                     next_state = torch.FloatTensor(next_state).to(agent1.device)
                     next_state_for_disc = next_state[:,0:-args.num_skills]
@@ -117,19 +127,25 @@ for i_episode in itertools.count(1):
                     discriminator_optim.zero_grad()
                     discriminator_loss.backward()
                     discriminator_optim.step()
-
-                writer.add_scalar('loss/critic_1_0', critic_1_loss_0, updates)
-                writer.add_scalar('loss/critic_2_0', critic_2_loss_0, updates)
-                writer.add_scalar('loss/policy_0', policy_loss_0, updates)
-                writer.add_scalar('loss/entropy_loss_0', ent_loss_0, updates)
-                writer.add_scalar('entropy_temprature/alpha_0', alpha_0, updates)
-                if args.diayn:
-                    writer.add_scalar('loss/discriminator_loss', discriminator_loss, updates)
+                if updates % 1000 == 0:
+                    writer.add_scalar('loss/critic_1_0', critic_1_loss_0, updates)
+                    writer.add_scalar('loss/critic_2_0', critic_2_loss_0, updates)
+                    writer.add_scalar('loss/policy_0', policy_loss_0, updates)
+                    writer.add_scalar('loss/entropy_loss_0', ent_loss_0, updates)
+                    writer.add_scalar('entropy_temprature/alpha_0', alpha_0, updates)
+                    if args.diayn and i_episode > args.disc_start_epi:
+                        writer.add_scalar('loss/discriminator_loss', discriminator_loss, updates)
                 updates += 1
 
         next_state, reward, done, _ = env.step(action) # Step
         episode_steps += 1
         total_numsteps += 1
+        if args.diayn:
+            with torch.no_grad():
+                skills     = torch.LongTensor([skill]).to(agent1.device)
+                next_state_for_disc = torch.FloatTensor(next_state).unsqueeze(0).to(agent1.device)
+                logits     = discriminator(next_state_for_disc)
+                reward     = (-F.cross_entropy(logits, skills) - np.log(1/args.num_skills + 1e-6) -np.log(1/2. + 1E-6)).item()
         episode_reward += reward
 
         # Ignore the "done" signal if it comes from hitting the time horizon.
@@ -147,10 +163,11 @@ for i_episode in itertools.count(1):
         break
     
     skill_str =  "" if not args.diayn else str(skill)
-    writer.add_scalar('reward/train_'+skill_str, episode_reward, i_episode)
-    print("Episode: {}, skill: {}, total numsteps: {}, episode steps: {}, reward: {}".format(i_episode, skill_str, total_numsteps, episode_steps, round(episode_reward, 2)))
+    agent_id_str = "" if not args.diayn else str(agent_id)
+    writer.add_scalar('reward/train_'+skill_str+'_'+agent_id_str, episode_reward, i_episode)
+    print("Episode: {}, skill: {}, agent: {}, total numsteps: {}, episode steps: {}, reward: {}".format(i_episode, skill_str, agent_id_str, total_numsteps, episode_steps, round(episode_reward, 2)))
 
-    if i_episode % 100 == 0:
+    if i_episode % 2000 == 0:
         agent0.save_model(args.env_name,'adiayn_0_'+str(i_episode))
         agent1.save_model(args.env_name,'adiayn_1_'+str(i_episode))
 
