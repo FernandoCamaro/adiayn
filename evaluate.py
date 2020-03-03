977b038fb7066ea818c63ac8a6461f52117a50d7
Fernando Camaro Nogues <fernando@ascent.ai>
2020-03-04 00:34:56 +0900

    modify evaluate properly

--- evaluate.py
+++ evaluate.py
@@ -4,23 +4,35 @@ import gym
 import torch
 import numpy as np
 from matplotlib import animation
+import pybullet
+import pybullet_envs
+import time
 
-actor_path = "models/sac_actor_Pendulum-v0_adiayn_1_900.pth" # 700
-env_name = 'Pendulum-v0'
+# actor_path = "models/sac_actor_BipedalWalkerHardcore-v3_adiayn_1_8000.pth" # 700
+# env_name = 'BipedalWalkerHardcore-v3'
+# env_name = 'Pendulum-v0'
+env_name = 'AntBulletEnv-v0'
+# actor_path = "models/sac_actor_Pendulum-v0_adiayn_0_4000.pth" # 700
+# actor_path = "models/sac_actor_AntBulletEnv-v0_adiayn_0_24000.pth"
+actor_path = "/home/fernando/Downloads/adiayn/sac_actor_AntBulletEnv-v0_adiayn_0_1329.pth"
+
+
 cuda = True
 seed = 123456
-env = gym.make(env_name)
-env.seed(seed)
-env._max_episode_steps = 500
+env = gym.make(env_name, render="human")
+# env.seed(seed)
+env._max_episode_steps = 1000
 num_skills = 2
 skill = 1
+pybullet_env = "Bullet" in env_name
 
 num_inputs = env.observation_space.shape[0]
 action_space = env.action_space
-hidden_size = 64#256
+hidden_size = 256
 device = torch.device("cuda" if cuda else "cpu")
 policy = GaussianPolicy(num_inputs+num_skills, action_space.shape[0], hidden_size, action_space).to(device)
 policy.load_state_dict(torch.load(actor_path))
+policy.eval()
 
 def display_frames_as_gif(frames):
     """
@@ -31,19 +43,28 @@ def display_frames_as_gif(frames):
     plt.axis('off')
 
     def animate(i):
+        print(frames[i].shape)
         patch.set_data(frames[i])
 
     anim = animation.FuncAnimation(plt.gcf(), animate, frames = len(frames), interval=300)
     #display(anim)
 
+time.sleep(2)
 state = env.reset()
+time.sleep(2)
+# if pybullet_env:
+#     print("sleep")
+#     pybullet.setRealTimeSimulation(1)
+#     time.sleep(2)
+#     pybullet.setRealTimeSimulation(0)
 cum_reward = 0
 frames = []
-max_steps = 9000
-policy.eval()
+max_steps = 250
+# policy.eval()
 z_one_hot = np.zeros(num_skills)
 z_one_hot[skill] = 1.
 for t in range(max_steps):
+    print(state)
     state = np.concatenate((state,z_one_hot))
     # Render into buffer. 
     frames.append(env.render(mode = 'rgb_array'))
@@ -59,4 +80,9 @@ print(done, t)
 print(cum_reward)
 env.close()
 display_frames_as_gif(frames)
+
+from PIL import Image
+for i, frame in enumerate(frames):
+    img = Image.fromarray(frame)
+    img.save("/tmp/"+str(i).zfill(3)+'.png')
 
